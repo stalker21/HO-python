1cf7da9f90aae542cf6e0cf0b1526ac79ac86eb8
stalker21 <leonardosalzano@gmail.com>
2017-11-27 15:29:47 -0300

    cambios al 27-11

--- factores_primos.py
+++ factores_primos.py
@@ -1,20 +1,20 @@
-x= 2
+x= 2 #definicion de variables
 numero = 600851475143
-suma = 0
-factoresprimos = []
+
+factoresprimos = [] #definicion de lista
 
 
-while (numero != 1):
+while (numero != 1): #evaluo si numero es disntito de 1
 
-    if (numero % x == 0):
-        factoresprimos.append(x)
+    if (numero % x == 0): #evaluo si x es factor primo de numero
+        factoresprimos.append(x) #inserto x en lista
     
-        numero = numero/x
-        suma +=1
+        numero = numero/x #actualizo numero
+        
     else:
-        x = x + 1
+        x = x + 1 #actualizo x si no es factor primo de numero
 
-print factoresprimos
+print factoresprimos #imprimo lista factores primos
 
-maxfactorprimo=max(factoresprimos)
-print maxfactorprimo
+maxfactorprimo=max(factoresprimos) #evaluo max factor primo en lista
+print maxfactorprimo #imprimo max factor primo
--- fibonacci.py
+++ fibonacci.py
@@ -1,11 +1,11 @@
-a=1
+a=1 #defino variables
 b=2
 suma=0
 
-while b<=1000000:
-    if b%2 != 0:
-        suma += b
-    temp=b
-    b= b+a
-    a= temp
-print suma
+while b<=1000000: #evaluo que b sea menor o igual que 1000000
+    if b%2 != 0: #evaluo que b sea multiplo de 3 o 5
+        suma += b #calculo la suma de los multiplos de 3 y 5
+    temp=b #si no es multiplo guardo en temp
+    b= b+a #actualizo b
+    a= temp #actualizo a
+print suma #imprimo suma multiplos 3 y 5
